f8fac2b8cdeab9fe917da90e71a039c34ac21a56
dourgulf <dawenhing@gmail.com>
2017-07-17 11:27:00 +0800

    Initial Commit

---
+++ jcdispatch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jcdispatch
+//
+//  Created by dawenhing on 17/07/2017.
+//  Copyright © 2017 dawenhing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
